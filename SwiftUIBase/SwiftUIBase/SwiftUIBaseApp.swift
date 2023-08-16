744126507165de51da516e9ee26adcd00821e63d
manjil <manjilrabhandari@gmail.com>
2023-08-16 13:11:02 +0545

    feat (main): create project

---
+++ SwiftUIBase/SwiftUIBase/SwiftUIBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBaseApp.swift
+//  SwiftUIBase
+//
+//  Created by manjil on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
